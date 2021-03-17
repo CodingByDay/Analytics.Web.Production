a98d6cd821e1ac5a8efafbaaac21f89bdf67242b
Janko Jovičič <janko@trendnet.local>
2021-03-17 11:52:08 +0100

    Adding the admin button

--- Site.Master.designer.cs
+++ Site.Master.designer.cs
@@ -24,13 +24,13 @@ namespace peptak
         protected global::System.Web.UI.WebControls.Button cmdSignOut;
 
         /// <summary>
-        /// Button1 control.
+        /// admin control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Button Button1;
+        protected global::System.Web.UI.WebControls.Button admin;
 
         /// <summary>
         /// MainContent control.
