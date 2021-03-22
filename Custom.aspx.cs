a421f6536802cec1e0a591b245512009286dd827
Janko <jankojovicic351@gmail.com>
2021-03-22 09:26:20 +0100

    Adding custom storage

--- Custom.aspx.cs
+++ Custom.aspx.cs
@@ -1,7 +1,10 @@
-﻿using System;
+﻿using DevExpress.DashboardWeb;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
+using System.Web.Security;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using WebDesigner_CustomDashboardStorage;
@@ -11,10 +14,50 @@ namespace peptak
     public partial class Custom : System.Web.UI.Page
     {
         static CustomDashboardStorage dashboardStorage = new CustomDashboardStorage();
+        private SqlConnection conn;
+        private SqlCommand cmd;
+
         protected void Page_Load(object sender, EventArgs e)
         {
+            var folder = HttpContext.Current.User.Identity.Name;
 
-           // DevExpress.DashboardWeb.ASPxDashboard2.SetDashboardStorage(dashboardStorage);
+            ASPxDashboard2.DashboardStorageFolder = $"~/App_Data/{folder}";
+            // Custom data storage. 
+
+            ASPxDashboard2.SetConnectionStringsProvider(new DevExpress.DataAccess.Web.ConfigFileConnectionStringsProvider());
+            string uname = HttpContext.Current.User.Identity.Name;
+            conn = new SqlConnection("server=10.100.100.25\\SPLAHOST;Database=petpakDash;Integrated Security=false;User ID=petpakn;Password=net123321!;");
+            conn.Open();
+            // Create SqlCommand to select pwd field from users table given supplied userName.
+            cmd = new SqlCommand($"select userRole from Users where uname='{uname}'", conn); /// Intepolation or the F string. C# > 5.0       
+            // Execute command and fetch pwd field into lookupPassword string.
+            var sdr = cmd.ExecuteScalar();
+
+            var user = sdr.ToString();
+
+            if (user != "Manager")
+            {
+                ASPxDashboard2.WorkingMode = WorkingMode.Viewer;
+            }
+            else
+            {
+                ASPxDashboard2.WorkingMode = WorkingMode.Designer;
+            }
+
         }
+/// <summary>
+/// 
+/// </summary>
+/// <param name="sender"></param>
+/// <param name="e"></param>
+
+
+        protected void cmdSignOut_Click(object sender, EventArgs e)
+        {
+            FormsAuthentication.SignOut();
+            Response.Redirect("logon.aspx", true);
+
+        }
+
     }
 }
