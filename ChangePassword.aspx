909db4938d6edc7aed42998ef19ac49c999f0259
Janko <jankojovicic351@gmail.com>
2021-04-07 10:09:19 +0200

    Adding the reset token

---
+++ ChangePassword.aspx
@@ -0,0 +1,76 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="ChangePassword.aspx.cs" Inherits="peptak.ChangePassword" %>
+
+<!DOCTYPE html>
+<html>
+   <head>
+      <title>Resetiraj geslo.</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <link href="https://fonts.googleapis.com/css?family=Nunito+Sans:300i,400,700&display=swap" rel="stylesheet">
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+      <style>
+         body {
+         background-color: #17c0eb;
+         font-family: Nunito Sans;
+         }
+         .btn {
+         background-color: #17c0eb;
+         width: 100%;
+         color: #fff;
+         padding: 10px;
+         font-size: 18px;
+         }
+         .btn:hover {
+         background-color: #2d3436;
+         color: #fff;
+         }
+         input {
+         height: 50px !important;
+         }
+         .form-control:focus {
+         border-color: #18dcff;
+         box-shadow: none;
+         }
+         h3 {
+         color: #17c0eb;
+         font-size: 36px;
+         }
+         .cw {
+         width: 35%;
+         }
+         @media(max-width: 992px) {
+         .cw {
+         width: 60%;
+         }
+         }
+         @media(max-width: 768px) {
+         .cw {
+         width: 80%;
+         }
+         }
+         @media(max-width: 492px) {
+         .cw {
+         width: 90%;
+         }
+         }
+      </style>
+   </head>
+   <body>
+      <div class="container d-flex justify-content-center align-items-center vh-100">
+         <div class="bg-white text-center p-5 mt-3 center">
+            <h3>Spremeni geslo. </h3>
+            <p>Če ste pozabili geslo, lahko ga resetirate.</p>
+            <form class="pb-3" action="#" runat="server">
+               <div class="form-group">
+                   <asp:TextBox ID="pwd" runat="server" placeholder="Geslo" CssClass="form-control form-control-lg"></asp:TextBox>
+                   <asp:TextBox ID="REpwd" runat="server" placeholder="Še enkrat" CssClass="form-control form-control-lg"></asp:TextBox>
+
+               </div>
+           
+          <asp:Button ID="change" runat="server" Text="Spremeni" type="submit" OnClick="change_Click" CssClass="btn" />
+          <asp:Button ID="backButton" runat="server" Text="Začetna strana" type="submit"  OnClick="backButton_Click" CssClass="btn" />
+
+            </form>
+         </div>
+      </div>
+   </body>
+</html>
--- ResetPassword.aspx.cs
+++ ResetPassword.aspx.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
+using System.Net.Mail;
+using System.Text;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
@@ -9,6 +12,8 @@ namespace peptak
 {
     public partial class ResetPassword : System.Web.UI.Page
     {
+        private SqlConnection conn;
+
         protected void Page_Load(object sender, EventArgs e)
         {
 
@@ -16,12 +21,74 @@ namespace peptak
 
         protected void reset_Click(object sender, EventArgs e)
         {
+            CheckIfUserExists();
+            SendEmail();
+            UpdateVisualDesign();
+        }
+
+        private void UpdateVisualDesign()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void SendEmail()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void CheckIfUserExists()
+        {
+            conn = new SqlConnection("server=10.100.100.25\\SPLAHOST;Database=graphs;Integrated Security=false;User ID=petpakn;Password=net123321!;");
+            string sendPassword = String.Format($"select uname, Pwd FROM Users where email='{email.Text}'");
+            SqlCommand createUser = new SqlCommand(finalQueryRegistration, conn);
+            var username = TxtUserName.Text;
+            try
+            {
+                createUser.ExecuteNonQuery();
+            }
 
         }
+
+
 
         protected void backButton_Click(object sender, EventArgs e)
         {
             Response.Redirect("logon.aspx", true);
         }
+
+
+
+        private void SendPasswordResetEmail(string ToEmail, string UserName, string UniqueId)
+        {
+            // MailMessage class is present is System.Net.Mail namespace
+            MailMessage mailMessage = new MailMessage("YourEmail@gmail.com", ToEmail);
+
+
+            // StringBuilder class is present in System.Text namespace
+            StringBuilder sbEmailBody = new StringBuilder();
+            sbEmailBody.Append("Dear " + UserName + ",<br/><br/>");
+            sbEmailBody.Append("Prosim sledite link da resetirate geslo.");
+            sbEmailBody.Append("<br/>"); sbEmailBody.Append("http://localhost/ChangePassword.aspx?uid=" + UniqueId);
+            sbEmailBody.Append("<br/><br/>");
+            sbEmailBody.Append("<b>IN SIST doo</b>");
+
+            mailMessage.IsBodyHtml = true;
+
+            mailMessage.Body = sbEmailBody.ToString();
+            mailMessage.Subject = "Resetirajte geslo.";
+            SmtpClient smtpClient = new SmtpClient("smtp.gmail.com", 587);
+
+            smtpClient.Credentials = new System.Net.NetworkCredential()
+            {
+                UserName = "YourEmail@gmail.com",
+                Password = "YourPassword"
+            };
+
+            smtpClient.EnableSsl = true;
+            smtpClient.Send(mailMessage);
+        }
+
+
+
     }
 }
