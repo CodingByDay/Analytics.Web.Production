14dce31fc4e89c13c8b24cc771b4c2ec9b0748db
Janko <jankojovicic351@gmail.com>
2021-04-11 18:31:32 +0200

    Working json object

--- Success.aspx.cs
+++ Success.aspx.cs
@@ -1,4 +1,6 @@
 ﻿using peptak.Session;
+using Stripe;
+using Stripe.Checkout;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,13 +16,34 @@ namespace peptak
 
         protected void Page_Load(object sender, EventArgs e)
         {
-          
-
-          var id = Request.QueryString["id"];
 
 
+            // Stripe configuration key. Safe to use like this...
+            StripeConfiguration.ApiKey = "sk_test_51IdsgFI2g0bX7R9aOa1atoifUOkT6csUhopE53KQYUA55UZmKVPdWP3BLcZ9UTqF6zVIn0OIpeAXlL5CiiLHFWv900tcoe7Bzt";
+            // Getting the query string.
+            var id = Request.QueryString["id"];
 
+
+            // testing the query string.
             Response.Write(id.ToString());
+
+
+
+            /// Page that will show all the data for the session and also write info to the database.
+            /// 
+            var service = new SessionService();
+
+
+
+            var session = service.Get(id);
+
+
+
+            Response.Write(session.ToJson());
+            //
+
+
+
 
 
 
