9d5f50b05481ac95e7411bb83a35622f5ec726f6
Janko Jovičić <jankojovicic351@gmail.com>
2022-04-12 11:31:46 +0200

    Dynamic dates.

---
+++ js/application/admin.js
@@ -0,0 +1,284 @@
+﻿initialPayload = [];
+updatedPayload = [];
+/**
+ * A client side event to update the column header titles based on parameter values.
+ * @param sender
+ * @param args
+ */
+
+
+
+
+
+function regex_return(text_to_search) {
+    var re = /(?:^|\W)#(\w+)(?!\w)/g, match, matches = [];
+    while (match = re.exec(text_to_search)) {
+        matches.push(match[1]);
+    }
+    return matches;
+}
+
+
+
+
+
+
+function customizeWidgets(sender, args) {
+    var parName = []
+    var collection = dashboard.GetParameters().GetParameterList();
+    if (args.ItemName.startsWith("gridDashboardItem") && collection.length > 2) {
+        initialPayload.push(dashboard.GetParameters().GetParameterList()[0].Value);
+        initialPayload.push(dashboard.GetParameters().GetParameterList()[1].Value);
+        initialPayload.push(dashboard.GetParameters().GetParameterList()[2].Value);
+        initialPayload.push(dashboard.GetParameters().GetParameterList()[3].Value);
+
+
+        parName.push(dashboard.GetParameters().GetParameterList()[0].Name);
+        parName.push(dashboard.GetParameters().GetParameterList()[1].Name);
+        parName.push(dashboard.GetParameters().GetParameterList()[2].Name);
+        parName.push(dashboard.GetParameters().GetParameterList()[3].Name);
+
+
+        var grid = args.GetWidget();
+
+        var columns = grid.option("columns");
+        for (var i = 0; i < columns.length; i++) {
+            var textToCheck = columns[i].caption;
+            window.textNew = textToCheck;
+            var parameterized_values = regex_return(textToCheck);
+            if (parameterized_values.length != 0) {
+
+                     // for each loop for every found parameter
+                   parameterized_values.forEach((singular) => {
+
+                   const found = parName.find(element => element == singular)
+
+                   indexOfElement = parName.indexOf(found)
+
+                       if (found != null && indexOfElement != -1) {
+                           
+                           text_to_replace = "#" + found
+                           text_replace = dashboard.GetParameters().GetParameterList()[indexOfElement].Value.toLocaleDateString("uk-Uk")
+                           window.textNew = window.textNew.replace(text_to_replace, text_replace);
+                           console.log(window.textNew)
+                           columns[i].caption = window.textNew;
+                    } else {
+                       
+                    }
+                })
+            } else {
+              
+            }
+
+        }
+        grid.option("columns", columns);
+    }
+}
+
+
+function updatecustomizeWidgets(sender, args) {
+    var parName = []
+    var collection = dashboard.GetParameters().GetParameterList();
+
+    if (args.ItemName.startsWith("gridDashboardItem") && collection.length > 2) {
+
+        initialPayload = [];
+        initialPayload.push(dashboard.GetParameters().GetParameterList()[0].Value);
+        initialPayload.push(dashboard.GetParameters().GetParameterList()[1].Value);
+        initialPayload.push(dashboard.GetParameters().GetParameterList()[2].Value);
+        initialPayload.push(dashboard.GetParameters().GetParameterList()[3].Value);
+
+
+        parName.push(dashboard.GetParameters().GetParameterList()[0].Name);
+        parName.push(dashboard.GetParameters().GetParameterList()[1].Name);
+        parName.push(dashboard.GetParameters().GetParameterList()[2].Name);
+        parName.push(dashboard.GetParameters().GetParameterList()[3].Name);
+
+       
+        var grid = args.GetWidget();
+        var columns = grid.option("columns");
+        for (var i = 0; i < columns.length; i++) {
+            var textToCheck = columns[i].caption;
+            window.textNew = textToCheck;
+
+            var parameterized_values = regex_return(textToCheck);
+            if (parameterized_values.length != 0) {
+
+                // for each loop for every found parameter
+                parameterized_values.forEach((singular) => {
+
+                    const found = parName.find(element => element == singular)
+
+                    indexOfElement = parName.indexOf(found)
+
+                    if (found != null && indexOfElement != -1) {
+
+                        text_to_replace = "#" + found
+                        text_replace = dashboard.GetParameters().GetParameterList()[indexOfElement].Value.toLocaleDateString("uk-Uk")
+                        window.textNew = window.textNew.replace(text_to_replace, text_replace);
+                        console.log(window.textNew)
+                        columns[i].caption = window.textNew;
+                    } else {
+                     
+                    }
+                })
+            } else {
+               
+            }
+        }
+        grid.option("columns", columns);
+    }
+}
+
+
+
+
+payload = [];
+
+
+
+var extension;
+
+/**
+ *  
+ * @param sender
+ */
+
+function onBeforeRender(sender) {
+
+    var dashboardControl = sender.GetDashboardControl();
+    extension = new DevExpress.Dashboard.DashboardPanelExtension(dashboardControl);
+    dashboardControl.surfaceLeft(extension.panelWidth);
+    dashboardControl.registerExtension(extension);
+
+}
+
+function setCookie(cname, cvalue, exdays) {
+    const d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    let expires = "expires=" + d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+/**
+ * Getting the cookie value.
+ * @param cname
+ */
+
+function getCookie(cname) {
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for (let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+
+
+$(document).keypress(function (e) { if (e.keyCode === 13) { e.preventDefault(); return false; } });
+
+$(function () {
+
+    $(':text').bind('keydown', function (e) { // on keydown for all textboxes  
+
+        if (e.keyCode == 13) // if this is enter key  
+
+            e.preventDefault();
+
+    });
+
+});
+
+/**
+ * Change the visibility of the collapsable hamburger menu. */
+function toggleVisibilityHide(toHide) {
+
+    var picture = document.getElementById("pic")
+    if (toHide == true) {
+
+        picture.style.visibility = "hidden"
+    } else {
+        picture.style.visibility = "visible"
+    }
+}
+
+/* Jquery function to handle hamburger clicked */
+$(document).ready(function () {
+    $("#pic").mouseover(function () {
+
+        var expand = getCookie("expand");
+        if (expand == "true") {
+            onExpand();
+        } else {
+            var control = dashboard.GetDashboardControl();
+            design = control.isDesignMode();
+
+            if (design == false) {
+                onCollapse();
+            }
+        }
+
+
+    });
+
+});
+
+function show() {
+    $('.dx-overlay-content').show();
+    console.log("Show");
+    $(".dx-dashboard-surface").attr('style', 'left: 250px !important');
+    changePicStateHideIt(true);
+
+}
+
+
+function hide() {
+
+    $('.dx-overlay-content').hide();
+    console.log("hide");
+    $(".dx-dashboard-surface").attr('style', 'left: 10px !important');
+    changePicStateHideIt(false);
+
+}
+
+function onExpand() {
+
+    var control = dashboard.GetDashboardControl();
+    extension.showPanelAsync({}).done(function (e) {
+
+        control.surfaceLeft(e.surfaceLeft);
+        setCookie("expand", "false", 365);
+
+    });
+}
+
+
+
+
+function onCollapse() {
+
+    var control = dashboard.GetDashboardControl();
+    extension.hidePanelAsync({}).done(function (e) {
+        control.surfaceLeft(e.surfaceLeft);
+        toggleVisibilityHide(false);
+        setCookie("expand", "true", 365);
+
+    });
+}
+
+function correctTheLoadingState() {
+    var control = dashboard.GetDashboardControl();
+
+    design = control.isDesignMode();
+    if (design == false) {
+        onCollapse();
+    }
+}
---
+++ obj/Release/Package/PackageTmp/ChangePassword.aspx
@@ -0,0 +1,86 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="ChangePassword.aspx.cs" Inherits="Dash.ChangePassword" %>
+
+<!DOCTYPE html>
+<html>
+   <head>
+      <title>Resetiraj geslo.</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <link href="https://fonts.googleapis.com/css?family=Nunito+Sans:300i,400,700&display=swap" rel="stylesheet">
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+       <script src="https://js.stripe.com/v3/"></script>
+      <style>
+         body {
+         background-color: #17c0eb;
+         font-family: Nunito Sans;
+         }
+         .btn {
+         background-color: #17c0eb;
+         width: 100%;
+         color: #fff;
+         padding: 10px;
+         font-size: 18px;
+         }
+         .btn:hover {
+         background-color: #2d3436;
+         color: #fff;
+         }
+         input {
+         height: 50px !important;
+         }
+         .form-control:focus {
+         border-color: #18dcff;
+         box-shadow: none;
+         }
+         h3 {
+         color: #17c0eb;
+         font-size: 36px;
+         }
+         .cw {
+         width: 35%;
+         }
+         @media(max-width: 992px) {
+         .cw {
+         width: 60%;
+         }
+         }
+         @media(max-width: 768px) {
+         .cw {
+         width: 80%;
+         }
+         }
+         @media(max-width: 492px) {
+         .cw {
+         width: 90%;
+         }
+         }
+      </style>
+   </head>
+   <body>
+                  <div class="wrapper fadeInDown">
+
+      <div class="container d-flex justify-content-center align-items-center vh-100">
+         <div class="bg-white text-center p-5 mt-3 center">
+            <h3>Spremeni geslo. </h3>
+            <form class="pb-3" action="#" runat="server">
+               <div class="form-group">
+                   
+                       <center> <asp:TextBox ID="pwd" runat="server"  
+                                     TextMode="Password" placeholder="Geslo" CssClass="form-control form-control-lg"></asp:TextBox>  </center>
+                   
+                       <center> <asp:TextBox ID="REpwd" runat="server"  
+                                     TextMode="Password" placeholder="Še enkrat" CssClass="form-control form-control-lg"></asp:TextBox>  </center>
+             
+
+               </div>
+           
+          <asp:Button ID="change" runat="server" Text="Spremeni" type="submit" OnClick="change_Click" CssClass="btn" />
+                <br />
+                <br />
+          <asp:Button ID="backButton" runat="server" Text="Začetna strana" type="submit"  OnClick="backButton_Click" CssClass="btn" />
+
+            </form>
+             </div>
+         </div>
+      </div>
+   </body>
+</html>
