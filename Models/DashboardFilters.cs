f9a62c3a3f0bfd12f492906c573d25272c72df80
Janko Jovičić <jankojovicic351@gmail.com>
2024-10-08 12:17:57 +0200

    Saving the filters start.

---
+++ Models/DashboardFilters.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+
+namespace Dash.Models
+{
+    public class DashboardFilters
+    {
+        [JsonIgnore]
+        public string Connection = ConfigurationManager.ConnectionStrings["graphsConnectionString"].ConnectionString;
+
+        public List<DashboardFilter> Filters { get; set; } = new List<DashboardFilter>();
+    }
+
+
+
+
+    public class DashboardFilter
+    {
+        public int id { get; set; }
+
+
+    }
+}
